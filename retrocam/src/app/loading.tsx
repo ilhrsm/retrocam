2899c18e511d5b20f05a4a82af0b40e0eee6f10f
ilhrsm <nojjang199@gmail.com>
2026-07-20 16:46:41 +0900

    Delete retrocam directory

--- retrocam/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Aperture } from "@/components/ui/Aperture";
-
-export default function Loading() {
-  return (
-    <div className="flex h-[60vh] items-center justify-center">
-      <Aperture size={36} className="animate-spin text-ink/25" />
-    </div>
-  );
-}
